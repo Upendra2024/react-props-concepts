58a28e414b922a55fd6e8380e84b0f719b979976
Upendra2024 <92069507+Upendra2024@users.noreply.github.com>
2022-09-23 17:35:42 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
       <Greet name = "Batman"  studioName ="DC comics"/>
       <Message name ="SpiderMan" studioName="Marvel"/>
       <Eventbind/>
-      <Parent/>
+      {/* <Parent/> */}
       
     </div>
   );
